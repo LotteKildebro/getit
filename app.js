f4078fbe089b1775ee265569af3aecc1a9f99084
LotteKildebro <4514756@RTS-365.dk>
2018-02-05 08:40:07 +0100

    commit

--- app.js
+++ app.js
@@ -10,7 +10,21 @@ app.use(logger('dev'));
 //udover at lytte på port 3000, skal der stå i adresse baren : localhost:3000/api/test
 // så er det denne route vi rammer
 app.get('/api/getit', (req, res) => {
-    res.json({ navn: "Lotte", alder: "24"  });
+    res.json({ navn: "Lotte", efternavn: "Kildebro", alder: "24"  });
+ });
+//profile route
+ app.get('/api/profile', (req, res)=>{
+    res.json({ navn: 'pikachu', art: 'pokemon', level: '4'  });
+ });
+
+//kunster router
+app.get('/api/kunstner', (req, res)=>{
+    res.json({ navn: 'John lennon', født: '1940', band: 'beatles'  });
+ });
+
+ //test router
+ app.get('/api/test', (req, res)=>{
+    res.json({ tekst: 'hej verden'  });
  });
 
 //Beder server om at bruge 'express.static' , det skal være den
@@ -26,3 +40,5 @@ app.listen(3000, (err) => {
 //dette er for at vise om serveren virker..
     console.log('App is listening on http://localhost:3000');
  });
+
+
